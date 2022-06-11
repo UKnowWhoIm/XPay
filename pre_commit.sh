1efb2501119c03e4857bb81c5900c6efe7016e85
Sidharth <sidharthajithkumar@gmail.com>
2022-06-11 19:40:35 +0530

    fixed pre-commit

--- pre_commit.sh
+++ pre_commit.sh
@@ -5,15 +5,18 @@ echo "#!/usr/bin/env python3
 import subprocess
 import re
 
-changed_files = subprocess.run(["git", "--no-pager", "diff", "--name-only", "HEAD"], capture_output=True)
+changed_files = subprocess.run([\"git\", \"--no-pager\", \"diff\", \"--name-only\", \"HEAD\"], capture_output=True)
 
-changed_files = changed_files.stdout.decode("utf-8").split("\n")
+changed_files = changed_files.stdout.decode(\"utf-8\").split(\"\\\n\")
 
-target_files = [file.replace("server", "/app") for file in changed_files if re.match(r"^server/app/.+\.py", file)]
+target_files = [file.replace(\"server\", \"/app\") for file in changed_files if re.match(r\"^server/app/.+\.py\", file)]
 
-lint = subprocess.run(["docker-compose", "run", "server", "poetry", "run", "pylint", *target_files], capture_output=True)
+if len(target_files) == 0:
+    exit(0)
 
-print(lint.stdout.decode("utf-8"))
+lint = subprocess.run([\"docker-compose\", \"run\", \"server\", \"poetry\", \"run\", \"pylint\", *target_files], capture_output=True)
+
+print(lint.stdout.decode(\"utf-8\"))
 
 exit(lint.returncode)
 " > .git/hooks/pre-commit
